53bf36408dcf30cba31f7946e2cfcaf3aa0af8d1
dat-amos <amos.omofaiye@gmail.com>
2024-09-11 23:14:21 +0100

    Finished login screen and connection to next activity

--- app/src/main/java/com/example/justina/MainActivity.kt
+++ app/src/main/java/com/example/justina/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.justina
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.setPadding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var usernameInput : EditText
+    lateinit var passwordInput : EditText
+    lateinit var loginBtn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +25,26 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        usernameInput = findViewById(R.id.username)
+        passwordInput = findViewById(R.id.password)
+        loginBtn = findViewById(R.id.login_button)
+
+        loginBtn.setOnClickListener {
+            val username = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+            Log.i("Test Credentials", "Username : $username and Password : $password")
+            if (username == "Justina" && password == "justina2024") {
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            }
+            else {
+                val text = "Your details are inaccurate"
+                val duration = Toast.LENGTH_SHORT
+                Toast.makeText(this, text, duration).show()
+            }
+
         }
     }
 }
