bfde8b28e0c41d192eb857af627563b2abc5a1fb
dat-amos <amos.omofaiye@gmail.com>
2024-09-15 22:28:41 +0100

    Implemented carousel view for story activity with undesired up scroll rather than right scroll

--- app/src/main/java/com/example/justina/MainActivity2.kt
+++ app/src/main/java/com/example/justina/MainActivity2.kt
@@ -5,16 +5,49 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.justina.adapters.ImageAdapter
+import com.example.justina.models.ImageItem
+import java.util.UUID
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val imageRV = findViewById<RecyclerView>(R.id.imageRV)
+
+        val imageList = arrayListOf(
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/866/500/500.jpg?hmac=FOptChXpmOmfR5SpiL2pp74Yadf1T_bRhBF1wJZa9hg"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/270/500/500.jpg?hmac=MK7XNrBrZ73QsthvGaAkiNoTl65ZDlUhEO-6fnd-ZnY"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/320/500/500.jpg?hmac=2iE7TIF9kIqQOHrIUPOJx2wP1CJewQIZBeMLIRrm74s"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/798/500/500.jpg?hmac=Bmzk6g3m8sUiEVHfJWBscr2DUg8Vd2QhN7igHBXLLfo"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/95/500/500.jpg?hmac=0aldBQ7cQN5D_qyamlSP5j51o-Og4gRxSq4AYvnKk2U"
+            ),
+            ImageItem(
+                UUID.randomUUID().toString(),
+                "https://fastly.picsum.photos/id/778/500/500.jpg?hmac=jZLZ6WV_OGRxAIIYPk7vGRabcAGAILzxVxhqSH9uLas"
+            )
+        )
+
+        val imageAdapter = ImageAdapter()
+        imageRV.adapter = imageAdapter
+        imageAdapter.submitList(imageList)
+
     }
 }
---
+++ app/src/main/java/com/example/justina/models/ImageItem.kt
@@ -0,0 +1,8 @@
+package com.example.justina.models
+
+import java.sql.Struct
+
+data class ImageItem (
+    val id : String,
+    val url : String
+)
